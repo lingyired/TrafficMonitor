f40780a885ff5ee068c30accf9413d0152edfffd
lingyired <lingyired@gmail.com>
2023-06-05 08:55:26 +0800

    设置外频 1.01

--- OpenHardwareMonitorApi/OpenHardwareMonitorImp.cpp
+++ OpenHardwareMonitorApi/OpenHardwareMonitorImp.cpp
@@ -135,7 +135,7 @@ namespace OpenHardwareMonitorApi
         for (auto i : m_all_cpu_clock)
             sum += i.second;
         // 不超外频
-        freq = sum  / 6 / 1000.0 ;
+        freq = sum * 1.01  / 6 / 1000.0 ;
 
 
         return true;
